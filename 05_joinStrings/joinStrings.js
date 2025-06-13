014ad7da37c56c598f6a031728b6d7dd47c6be3d
Tyrone Tysdale-Montague <175446416+tyronetmGH@users.noreply.github.com>
2025-06-12 21:19:18 -0400

    fix: mirror example output to pass tests

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,7 +8,14 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+let thisYear = 1965;
+const birthYear = 1947;
+const fullName = (firstName + " " + lastName);
+let age = thisYear - birthYear;
 
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 
 
 
